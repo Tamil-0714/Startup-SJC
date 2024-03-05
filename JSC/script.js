c9ec0ff57ca5902afb690cc2bc3cbc2615334e8d
Tamil N <125873376+Tamil-0714@users.noreply.github.com>
2024-03-05 11:59:40 +0530

    Add files via upload

---
+++ JSC/script.js
@@ -0,0 +1,54 @@
+const submitBtn = document.querySelector(".submit-btn");
+const name = document.querySelector("#faculty");
+const fId = document.querySelector("#facultId");
+const dprt = document.querySelector("#dprt");
+const date = document.querySelector("#date");
+const venue = document.querySelector("#venue");
+
+submitBtn.addEventListener("click", handleWhatsAppSubmit);
+submitBtn.addEventListener("click", handleMailSubmit);
+
+function handleMailSubmit(e) {
+  e.preventDefault();
+  const jsonData = {
+    fName: name.value,
+    dprt: dprt.value,
+    date: date.value,
+    venue: venue.value,
+    FaucltId: fId.value,
+  };
+  const eMail = "tamil.tj.1967@gmail.com";
+  const mailTOUrl =
+    `mailto:${eMail}?subject="LED Display booking"&body= Facult Name: ${jsonData.fName}%0A%0A` +
+    `Facult Id: ${jsonData.FaucltId}%0A%0A` +
+    `Department: ${jsonData.dprt}%0A%0A` +
+    `Date: ${jsonData.date}%0A%0A` +
+    `Venue: ${jsonData.venue}`;
+
+  window.open(mailTOUrl);
+}
+
+function handleWhatsAppSubmit(e) {
+  e.preventDefault();
+  const jsonData = {
+    fName: name.value,
+    dprt: dprt.value,
+    date: date.value,
+    venue: venue.value,
+    FaucltId: fId.value,
+  };
+  const phoneNumber = "+919943112938";
+
+  const whatsappURL =
+    `https://wa.me/${phoneNumber}?text=Facult Name: ${jsonData.fName}%0A%0A` +
+    `Facult Id: ${jsonData.FaucltId}%0A%0A` +
+    `Department: ${jsonData.dprt}%0A%0A` +
+    `Date: ${jsonData.date}%0A%0A` +
+    `Venue: ${jsonData.venue}`;
+  +window.open(whatsappURL);
+}
+
+//   console.log(name.value);
+//   console.log(dprt.value);
+//   console.log(date.value);
+//   console.log(venue.value);
