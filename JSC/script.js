aa6e459ac27f73ef159e69698906ef854c26f4f2
Tamil-0714 <tom.jerry.07.05.06@gmail.com>
2024-03-06 10:04:39 +0530

    validation and animations added

--- JSC/script.js
+++ JSC/script.js
@@ -11,6 +11,7 @@ mail.addEventListener("click", handleMailSubmit);
 
 function handleMailSubmit(e) {
   e.preventDefault();
+  if (!vaidData()) return;
   const jsonData = {
     fName: name.value,
     dprt: dprt.value,
@@ -31,6 +32,7 @@ function handleMailSubmit(e) {
 
 function handleWhatsAppSubmit(e) {
   e.preventDefault();
+  if (!vaidData()) return;
   const jsonData = {
     fName: name.value,
     dprt: dprt.value,
@@ -48,6 +50,56 @@ function handleWhatsAppSubmit(e) {
     `Venue: ${jsonData.venue}`;
   +window.open(whatsappURL);
 }
+
+function vaidData() {
+  const nameVal = name.value.trim();
+  const fIdVal = fId.value.trim();
+  const dprtVal = dprt.value.trim();
+  const dateVal = date.value.trim();
+  const venueVal = venue.value.trim();
+
+  if (!nameVal) {
+    name.style.borderColor = "red";
+    return false;
+  } else {
+    name.style.borderColor = "black";
+  }
+  if (!fIdVal || fIdVal.length < 7) {
+    fId.style.borderColor = "red";
+    return false;
+  } else {
+    fId.style.borderColor = "black";
+  }
+  if (dprtVal === "none") {
+    dprt.style.borderColor = "red";
+    return false;
+  } else {
+    dprt.style.borderColor = "black";
+  }
+  if (!dateVal) {
+    date.style.borderColor = "red";
+    return false;
+  } else {
+    date.style.borderColor = "black";
+  }
+
+  if (venueVal === "none") {
+    venue.style.borderColor = "red";
+    return false;
+  } else {
+    venue.style.borderColor = "black";
+  }
+  return true;
+}
+
+// function validateHelper(e) {
+//   if (!e.value.trim()) {
+//     e.style.borderColor = "red";
+//     return false;
+//   }
+//   e.style.borderColor = "black";
+//   return true;
+// }
 
 //   console.log(name.value);
 //   console.log(dprt.value);
